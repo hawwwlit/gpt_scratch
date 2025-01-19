01632bbffc5708339208014f18edb88062437357
hawwwlit <hawwwlit@gmail.com>
2025-01-19 14:49:33 -0800

    some train code for mps

--- gpt_scratch.py
+++ gpt_scratch.py
@@ -9,14 +9,14 @@ import math
 # transformer: wte, wpe, (decoder blocks), layer norm
 # ffn
 # --
-# decoder: ln -> self attn -> ln -> mlp
+# decoder: ln -> self attn (residual) -> ln -> mlp (residual)
 
 class GPTConfig(BaseModel):
     vocab_size: int = 40478
     n_embed: int = 768
     n_layer: int = 12
     n_head: int = 12
-    seq_len: int = 512
+    max_seq_len: int = 1024
 
 class MLP(nn.Module):
     def __init__(self, config):
@@ -48,8 +48,8 @@ class CausalSelfAttention(nn.Module):
         self.n_head = config.n_head
         self.n_embed = config.n_embed
 
-        self.register_buffer('mask', torch.tril(torch.ones(config.seq_len, config.seq_len))
-                        .view(1, 1, config.seq_len, config.seq_len))
+        self.register_buffer('mask', torch.tril(torch.ones(config.max_seq_len, config.max_seq_len))
+                        .view(1, 1, config.max_seq_len, config.max_seq_len))
 
     def forward(self, x):
         batch_size, seq_len, n_embed = x.shape
@@ -63,7 +63,7 @@ class CausalSelfAttention(nn.Module):
 
         # Shape (B, nh, T, T)
         attn = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        attn = attn.masked_fill(self.mask[:,:,seq_len, seq_len] == 0, float('-inf'))
+        attn = attn.masked_fill(self.mask[:,:,:seq_len, :seq_len] == 0, float('-inf'))
         attn = F.softmax(attn, dim=-1)
 
         y = attn @ v # (B, nh, T, hs)
@@ -82,8 +82,8 @@ class Block(nn.Module):
         self.mlp = MLP(config)
 
     def forward(self, x):
-        x += self.attn(self.ln1(x))
-        x += self.mlp(self.ln2(x))
+        x = x + self.attn(self.ln1(x))
+        x = x + self.mlp(self.ln2(x))
         return x
 
 
@@ -93,15 +93,40 @@ class GPT(nn.Module):
 
         self.config = config
         self.transformer = nn.ModuleDict(dict(
-            tokens_embed = nn.Embedding(config.vocab_size, config.n_embd),
-            positions_embed = nn.Embedding(config.seq_len, config.n_embd),
+            tokens_embed = nn.Embedding(config.vocab_size, config.n_embed),
+            positions_embed = nn.Embedding(config.max_seq_len, config.n_embed),
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
-            ln = nn.LayerNorm(config.n_embd)
+            ln = nn.LayerNorm(config.n_embed)
         ))
-        self.ffn = nn.Linear(config.embed_dim, config.vocab_size, bias=False)
+        self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
+        
+        # weight sharing
+        self.transformer.tokens_embed.weight = self.lm_head.weight
+
+        # init weights
+        self.apply(self._init_weights)
+
+        for param_name, param in self.named_parameters():
+            if param_name.endswith('c_proj.weight'):
+                torch.nn.init.normal_(param, mean=0., std=0.02/math.sqrt(2*self.config.n_layer))
+        
+        n_params = sum(p.numel() for p in self.parameters())
+        print(f"Number of params: {n_params/1e6:.2f}M")
+    
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0., std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0., std=0.02)
+
 
     def forward(self, idx, targets=None):
+
         batch_size, seq_len = idx.size()
+
+        assert seq_len <= self.config.max_seq_len, f"Cannot forward sequence len {seq_len}, max allowed is {self.config.max_seq_len}" 
 
         tok_embed = self.transformer.tokens_embed(idx)
         pos = torch.arange(0, seq_len, dtype=torch.long, device=idx.device)
@@ -113,11 +138,112 @@ class GPT(nn.Module):
             x = block(x)
         
         x = self.transformer.ln(x)
-        logits = self.ffn(x) # (batch_size, seq_len, vocab_size)
+        logits = self.lm_head(x) # (batch_size, seq_len, vocab_size)
 
         loss = None
         if targets is not None:
+            # flatten out the logits tensor 
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1)) # F.cross_entropy expects the target tensor to be a 1D tensor of class indices
 
         return logits, loss
 
+device = 'cpu'
+if torch.cuda.is_available():
+    device = 'cuda'
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = 'mps'
+
+print(f"Using device: {device}")
+
+# set seed before model initialization! 
+torch.manual_seed(922)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(922)
+if hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    torch.mps.manual_seed(922)
+
+# initialize the model with random weights
+model = GPT(GPTConfig())
+model.to(device)
+# model = torch.compile(model)
+
+# a simple example
+import tiktoken
+import time
+
+class DataLoaderLite:
+    def __init__(self, input_file, B, T, enc='cl100k_base'):
+        enc = tiktoken.get_encoding(enc)
+        with open(input_file, 'r') as f:
+            text = f.read()
+        self.tokens = torch.tensor(enc.encode(text))
+        print(f"Loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B*T)} batches")
+        self.B = B
+        self.T = T
+        self.enc = enc
+        self.current_position = 0
+    
+    def next_batch(self):
+        buf = self.tokens[self.current_position: self.current_position+self.B*self.T+1]
+        x, y = buf[:-1].view(self.B, self.T), buf[1:].view(self.B, self.T)
+        self.current_position += self.B*self.T
+        if self.current_position+self.B*self.T> len(self.tokens):
+            self.current_position = 0
+        return x, y 
+
+torch.set_float32_matmul_precision('high')
+train_loader = DataLoaderLite('input.txt', B=8, T=1024)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    t0 = time.time()
+    optimizer.zero_grad()
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
+    with torch.autocast(device_type=device, dtype=torch.bfloat16):
+        logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    torch.mps.synchronize()
+    t1 = time.time()
+    dt = (t1-t0) * 1000
+    tokens_per_sec = (train_loader.B * train_loader.T) / (t1-t0)
+    
+    if i == 0:         # sanity check
+        print(f"expected initial loss is {-math.log(1/(train_loader.enc.max_token_value+1)):.3f} loss; actual is {loss.item():.3f}")
+    
+    print(f"Step: {i} | loss: {loss.item():.5f} | dt: {dt:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
+
+    if i % 10 == 0:
+        max_mem = torch.mps.recommended_max_memory()
+        curr_mem = torch.mps.current_allocated_memory()
+        print(f"Memory allocated: {curr_mem / (1024**2):.2f} MB | Max memory recommended: {max_mem / (1024**2):.2f} MB")
+
+
+import sys; sys.exit(0)
+batch_size = 5
+max_seq_len = 30
+tokens = enc.encode("Here in Seattle, ")
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(batch_size, 1)
+x = tokens.to(device)
+
+model.eval()
+t0 = time.time()
+while x.size()[-1] < max_seq_len:
+    with torch.no_grad():
+        logits, _ = model(x) # (b, t, vocab_size)
+        logits = logits[:, -1, :] # (b, vocab_size)
+        probs = F.softmax(logits, dim=-1)
+
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        ix = torch.multinomial(topk_probs, num_samples=1)
+        xcol = torch.gather(topk_indices, dim=1, index=ix) # returns the same result with dim=-1
+        x = torch.cat((x, xcol), dim=1)
+t1 = time.time()
+print(f"inference done after {(t1-t0)*1000:.3f} milliseconds")
+for i in range(batch_size):
+    tokens = x[i, :max_seq_len].tolist()
+    decoded = enc.decode(tokens)
+    print('>', decoded)
